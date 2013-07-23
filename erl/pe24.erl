32e29a82e89e75dbb603ae85e603a0d4fd7d971d
wanderwaltz <wanderwaltz@gmail.com>
2013-07-23 17:40:01 +0700

    Not computing the permutations which we certainly don't need.

--- erl/pe24.erl
+++ erl/pe24.erl
@@ -27,8 +27,24 @@ make_perm(List, Index) ->
 % Third param: list of available digits
 % Fourth param: number of digits to skip from the list when taking a new one to the permutation
 % Fifth param: permutation index starting from 0
-make_perm( found, Current,    _,    _,     _) -> {found, Current, 0}; % Stop recursion if found the answer
-make_perm(     _, Current,   [],    _,     0) -> {found, Current, 0}; % Stop recursion if index got the value 0 and digit pool is empty
+make_perm( found, Current,          _,    _,     _) -> {found, Current, 0}; % Stop recursion if found the answer
+make_perm(     _, Current,         [],    _,     0) -> {found, Current, 0}; % Stop recursion if index got the value 0 and digit pool is empty
+
+% Black optimization magic goes here. We don't actually need to compute the permutations if we certainly
+% know that these are not the droids we are looking for. We know that number of permutations of n elements
+% is n!, so we can just subtract the number from our Index parameter and continue without actually computing
+% the permutation values. I've hardcoded a couple of factorials to improve performance, though it can be
+% implemented more universally without need of hardcoding anything. Seems to be enough for making the 
+% whole solution 'instant' on my machine, so whatever.
+make_perm( _, _, List, _, Index) when length(List) == 9, Index > 362880 -> {not_found, [], Index - 362880}; % 9!
+make_perm( _, _, List, _, Index) when length(List) == 8, Index >  40320 -> {not_found, [], Index -  40320}; % 8!
+make_perm( _, _, List, _, Index) when length(List) == 7, Index >   5040 -> {not_found, [], Index -   5040}; % 7!
+make_perm( _, _, List, _, Index) when length(List) == 6, Index >    720 -> {not_found, [], Index -    720}; % 6!
+make_perm( _, _, List, _, Index) when length(List) == 5, Index >    120 -> {not_found, [], Index -    120}; % 5!
+make_perm( _, _, List, _, Index) when length(List) == 4, Index >     24 -> {not_found, [], Index -     24}; % 4!
+make_perm( _, _, List, _, Index) when length(List) == 3, Index >      6 -> {not_found, [], Index -      6}; % 3!
+make_perm( _, _, List, _, Index) when length(List) == 2, Index >      2 -> {not_found, [], Index -      2}; % 2!
+
 
 % Digit pool is empty, but the permutation index is not yet 0, so we've found some permutation, 
 % but had not found the right one yet. Decrement the index, but return 'not_found' status.
