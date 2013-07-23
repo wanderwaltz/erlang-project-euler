1e1093c8b4ed014d7e881ab529bca712f61fc4d1
wanderwaltz <wanderwaltz@gmail.com>
2013-07-23 18:05:41 +0700

    Updated pe7.erl to use primes module.

--- erl/pe7.erl
+++ erl/pe7.erl
@@ -4,11 +4,11 @@
 % By listing the first six prime numbers: 2, 3, 5, 7, 11, 
 % and 13, we can see that the 6th prime is 13.
 %
-% What is the 10 001st prime number?
+% What is the 10001st prime number?
 %--------------------------
 
 
-%=== BRUTE FORCE SOLUTION
+%=== BRUTE FORCE SOLUTION (more efficient solution can be found below)
 % Generate each prime until we reach 10001th.
 %
 % Primes are stored in a list and prime testing
@@ -45,8 +45,8 @@ divides_primes(X, [_|T]) -> divides_primes(X, T).
 % so we compute the list of primes less than 200000.
 
 solve_sieve() -> 
-    Sieve = pe10:sieve(200000),
-    lists:nth(length(Sieve)-10000, Sieve).
+    Sieve = primes:sieve(200000),
+    lists:nth(10001, Sieve).
 
 
 %=== BENCHMARK
