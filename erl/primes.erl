fdf9c5dee4334098517c83067e7fc73eb0a447d9
wanderwaltz <wanderwaltz@gmail.com>
2013-07-27 23:36:46 +0700

    Slightly optimized sieve of Eratosthenes implementation.

--- erl/primes.erl
+++ erl/primes.erl
@@ -1,5 +1,5 @@
 -module(primes).
--export([is_prime/2, sieve/1]).
+-export([is_prime/2, sieve/1, benchmark/0]).
 
 % Contains functions which could be useful for other Project Euler problems
 % and are related to prime numbers.
@@ -11,7 +11,7 @@ is_prime(         _,  _) 	 		 -> false.
 
 
 % Returns a list of primes <= N in ascending order
-sieve(N) -> lists:reverse(sieve_private([2], default_sieve(N))).
+sieve(N) -> lists:reverse(sieve_private([3|[2|[]]], N, default_sieve(N))).
 
 
 %------------------------------------------------------------------------------------
@@ -32,48 +32,51 @@ sieve(N) -> lists:reverse(sieve_private([2], default_sieve(N))).
 default_sieve(N) -> array:set(0, 0, array:set(1,0, array:new(N+1, {default,1}))).
 
 % We must provide an initial nonempty list for this function to work
-sieve_private(Primes, Sieve) ->
+sieve_private([not_found|Primes], _, _) -> Primes;
+sieve_private(Primes, N, Sieve) ->
 	% The current prime is expected to be the head of Primes list,
 	% that's why we must provide a nonempty initial value.
  	Prime  = hd(Primes),								
 
  	% Mark multipliers of the current prime in the array, 
- 	% but do not mark the prime itself (this actually is not
- 	% necessary to skip the prime itself since these values
- 	% are never used later, but whatever).
-	Marked = mark_multipliers(Sieve, Prime, Prime * 2),
+ 	% starting from the Prime^2. We don't have to check
+ 	% values between Prime and Prime^2 since these are 
+ 	% already marked when searching for previous primes.
+ 	Marked = mark_multipliers(N, Sieve, Prime, Prime * Prime),
 
 
 	% The next prime will be an index in array, whose value is still 1
-	Next   = find_1_after(Prime+1, Sieve),
-
-
-	% If there are no more 1s in the array, stop
-	if Next == {not_found} -> Primes;
-
-	% else continue, adding our next prime to the list.
-		true -> sieve_private([Next|Primes], Marked)
-	end.
+	Next   = find_1_after(Prime+2, N, Sieve),
+	sieve_private([Next|Primes], N, Marked).
 
 
 
 
 % Mark multipliers of the Value starting with Current in array Sieve
-mark_multipliers(Sieve, Value, Current) -> 
-	Size = array:size(Sieve),
-	if Current < Size -> mark_multipliers(array:set(Current, 0, Sieve), Value, Current+Value);
-		true -> Sieve
-	end.
+mark_multipliers(Size, Sieve, Value, Current) when Current < Size -> 
+	mark_multipliers(Size, array:set(Current, 0, Sieve), Value, Current+Value);
+mark_multipliers(_, Sieve, _, _) -> Sieve.
+
+%======
+% Find the first index after Candidate which has Sieve[Candidate] == 1
+% Do steps of 2 skipping even numbers
+
+% find_1_after/3 checks that the candidate for the next prime does not exceed
+% the limit and passes the Sieve[Candidate] to find_1_after/4
+find_1_after(Candidate, Limit, Sieve) when Candidate =< Limit -> find_1_after(Candidate, array:get(Candidate, Sieve), Limit, Sieve);
+find_1_after(_, _, _) -> not_found. % Return not_found when overflown the limit
+
+% find_1_after/4 checks whether the value passed as the second parameter is 1
+% (which would mean that the next prime number is found). If not, pass the next
+% prime candidate to find_1_after/3 for bounds checking.
+find_1_after(    Prime, 1, _, _) -> Prime;
+find_1_after(Not_Prime, _, Limit, Sieve) -> find_1_after(Not_Prime+2, Limit, Sieve).
 
 
-% Find the first index after I which has Sieve[I] == 1
-find_1_after(I, Sieve) ->
-	Size = array:size(Sieve),
-	if I < Size ->
-		Value = array:get(I, Sieve),
-		if Value == 1 -> I;
-			true -> find_1_after(I+1, Sieve)
-		end;
 
-		true -> {not_found}
-	end.
+%=== BENCHMARK
+
+benchmark() ->
+	io:format("Primes module benchmark: computing sieve(2000000). "),
+    {Time, Result} = timer:tc(?MODULE, sieve, [2000000]),
+    io:format("Time: ~p ms, found ~p primes~n", [Time/1000, length(Result)]).
